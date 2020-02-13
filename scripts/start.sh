6f448df7f3c7e131c53615f7a1dd0e15f0a4024c
dpvDuncan <dpvduncan@hotmail.fr>
2020-02-13 21:43:37 +0100

    Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,10 @@
 #! /bin/sh
+
+[ ! -f /config/settings.json ] && cp /var/lib/transmission/settings.json /config/
+
 chown -R $PUID:$PGID /var/lib/transmission /config
+
 sed -i "s/transmission:x:[0-9]\+:/transmission:x:$PUID:/" /etc/passwd
+sed -i "s/users:x:[0-9]\+:/users:x:$PGID:/" /etc/group
 
-GROUPNAME=$(getent group $PGID | cut -d: -f1)
-USERNAME=$(getent passwd $PUID | cut -d: -f1)
-
-if [ ! $GROUPNAME ]
-then
-        addgroup -g $PGID transmission
-        GROUPNAME=transmission
-fi
-
-if [ ! $USERNAME ]
-then
-        adduser -G $GROUPNAME -u $PUID -D transmission
-        USERNAME=transmission
-fi
-
-su - $USERNAME -c 'transmission-daemon -f -g /config'
+transmission-daemon -f -g /config
