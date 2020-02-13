86df2bba35712d8c8ff7e9a1204e027c4f5c5daf
dpvDuncan <dpvduncan@hotmail.fr>
2020-02-13 17:24:35 +0100

    Initial Commit

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ ARG BASE_IMAGE_PREFIX
 
 FROM multiarch/qemu-user-static as qemu
 
-FROM ${BASE_IMAGE_PREFIX}alpine:edge
+FROM ${BASE_IMAGE_PREFIX}alpine
 
 COPY --from=qemu /usr/bin/qemu-*-static /usr/bin/
 
@@ -10,14 +10,17 @@ ENV PUID=0
 ENV PGID=0
 
 COPY scripts/start.sh /
+COPY scripts/settings.json /var/lib/transmission/
 
 RUN apk -U --no-cache upgrade
-
-RUN rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/* /usr/bin/qemu-*-static
+RUN apk add --no-cache transmission-daemon
 RUN mkdir /config
 RUN chmod -R 777 /start.sh /config
+
+RUN rm -rf /usr/bin/qemu-*-static
+
 # ports and volumes
-EXPOSE 0
+EXPOSE 9091 51414 51414/udp
 VOLUME /config
 
-CMD [""]
+CMD ["/start.sh"]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,20 @@
 #! /bin/sh
-chown -R $PUID:$PGID /config
+chown -R $PUID:$PGID /var/lib/transmission /config
+sed -i "s/transmission:x:[0-9]\+:/transmission:x:$PUID:/" /etc/passwd
 
 GROUPNAME=$(getent group $PGID | cut -d: -f1)
 USERNAME=$(getent passwd $PUID | cut -d: -f1)
 
 if [ ! $GROUPNAME ]
 then
-        addgroup -g $PGID <groupname>
-        GROUPNAME=<groupname>
+        addgroup -g $PGID transmission
+        GROUPNAME=transmission
 fi
 
 if [ ! $USERNAME ]
 then
-        adduser -G $GROUPNAME -u $PUID -D <username>
-        USERNAME=<username>
+        adduser -G $GROUPNAME -u $PUID -D transmission
+        USERNAME=transmission
 fi
 
-su - $USERNAME -c ''
+su - $USERNAME -c 'transmission-daemon -f -g /config'
