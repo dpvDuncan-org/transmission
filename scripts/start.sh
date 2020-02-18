224bd8a0f4dd6047b59901f603fcc515a1b8da71
dpvDuncan <dpvduncan@hotmail.fr>
2020-02-18 11:07:22 +0100

    Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,26 @@
 
 [ ! -f /config/settings.json ] && cp /var/lib/transmission/settings.json /config/
 
-chown -R $PUID:$PGID /var/lib/transmission /config
+download_dir=$(jq -r .[\"download-dir\"] /config/settings.json)
+incomplete_dir_enabled=$(jq -r .[\"incomplete-dir-enabled\"] /config/settings.json)
+watch_dir_enabled=$(jq -r .[\"watch-dir-enabled\"] /config/settings.json)
 
-# sed -i "s/transmission:x:[0-9]\+:/transmission:x:$PUID:/" /etc/passwd
-# sed -i "s/users:x:[0-9]\+:/users:x:$PGID:/" /etc/group
+if [ "${incomplete_dir_enabled}" = "true" ]
+then
+        incomplete_dir=$(jq -r .[\"incomplete-dir\"] /config/settings.json)
+        [ ! -d ${incomplete_dir} ] && mkdir -p ${incomplete_dir}
+        chown -R $PUID:$PGID  ${incomplete_dir}
+fi
+if [ "${watch_dir_enabled}" = "true" ]
+then
+        watch_dir=$(jq -r .[\"watch-dir\"] /config/settings.json)
+        [ ! -d ${watch_dir} ] && mkdir -p ${watch_dir}
+        chown -R $PUID:$PGID  ${watch_dir}
+fi
+
+[ ! -d ${download_dir} ] && mkdir -p ${download_dir}
+
+chown -R $PUID:$PGID /var/lib/transmission /config ${download-dir}
 
 GROUPNAME=$(getent group $PGID | cut -d: -f1)
 USERNAME=$(getent passwd $PUID | cut -d: -f1)
